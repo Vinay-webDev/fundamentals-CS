88d51906483b1e6aa4b59f027277ef1ac22a38e1
Vinay-webDev <justchatting3000@gmail.com>
2025-01-10 20:22:24 +0530

    #1

---
+++ 1.cs
@@ -0,0 +1,32 @@
+Console.WriteLine('b');
+// Console.WriteLine('Hello World!');
+Console.WriteLine("Hello World!");
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+////////////////////////////////
+Console.WriteLine("///////////////////////////");
+Console.WriteLine("123");
+Console.WriteLine(123);
+Console.WriteLine("///////////////////////////");
+Console.WriteLine("true");
+Console.WriteLine(true);
+Console.WriteLine("///////////////////////////");
+///////////////////////////////////////////
+string firstName = "hello bro";
+Console.WriteLine(firstName);
+char firstLetter = 'h';
+// char firstLetter = 'hkd';
+Console.WriteLine(firstLetter);
+int number = 1000000000;
+Console.WriteLine(number);
+decimal number2 = 2.5m;
+Console.WriteLine(number2);
+double number3 = 10.1;
+Console.WriteLine(number3);
+bool pass = true;
+Console.WriteLine(pass);
+
